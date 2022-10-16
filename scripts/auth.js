632ef521a9b11e75f61ac761b33ad8c596ce12a0
Neagoe Dan Petre <dani.neagoe@yahoo.com>
2022-10-17 00:41:58 +0300

    Security, and new improves

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,20 +1,32 @@
-//get data
-/*db.collection('guides').get().then(snapshot => {
-  setupGuides(snapshot.docs);
-});*/
-
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    db.collection('guides').get().then(snapshot => {
+    db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
       setupUI(user);
-    });
+    }, err => console.log(err.message));
   } else {
     setupUI();
-    setupGuides([])
+    setupGuides([]);
   }
-})
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
 
 // signup
 const signupForm = document.querySelector('#signup-form');
@@ -25,8 +37,12 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
+  // sign up the user & add firestore data
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
+  }).then(() => {
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
